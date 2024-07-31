21f086a174fc6ec80c842e3a28080c6e6c66c657
DanaKLym <bohdanaklymenko@gmail.com>
2024-07-31 22:37:58 +0300

    added additional info to Footer

--- my-app/src/Footer/Footer.tsx
+++ my-app/src/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 const Footer: React.FC = () => {
   return (
     <>
-      <h3>Bohdana Klymenko</h3>
       <ul>
         <li>
           <a href="mailto:contact.kbohdana@gmail.com">
@@ -11,7 +10,16 @@ const Footer: React.FC = () => {
         <li>
           <a href="tel:+380680157956">+38 &#40;068&#41; 015-79-56</a>
         </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/bohdana-klymenko-254472176/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </li>
       </ul>
+      <h3>Bohdana Klymenko</h3>
     </>
   );
 };
