7547e145100860133d3b591fb6c10f5731ce1093
DanaKLym <bohdanaklymenko@gmail.com>
2024-07-31 17:50:36 +0300

    added some info to Footer

--- my-app/src/Footer/Footer.tsx
+++ my-app/src/Footer/Footer.tsx
@@ -1,7 +1,17 @@
 const Footer: React.FC = () => {
   return (
     <>
-      <p>This is a Footer</p>
+      <h3>Bohdana Klymenko</h3>
+      <ul>
+        <li>
+          <a href="mailto:contact.kbohdana@gmail.com">
+            contact.kbohdana@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="tel:+380680157956">+38 &#40;068&#41; 015-79-56</a>
+        </li>
+      </ul>
     </>
   );
 };
