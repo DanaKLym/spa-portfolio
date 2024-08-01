e784db2cd28ea751281c6adb9b1f072afdc89ce2
DanaKLym <bohdanaklymenko@gmail.com>
2024-08-01 11:40:21 +0300

    installed necessary dependencies to use SCSS, created SCSS file for Header

--- my-app/src/About/About.tsx
+++ my-app/src/About/About.tsx
@@ -4,7 +4,13 @@ const About: React.FC = () => {
   return (
     <>
       <Header />
-      <div>This is About sectio</div>
+      <p>
+        I'm a developer in training and still very much at the beginning of my
+        journey. It started with some projects created as a part of SheCodes
+        Workshops! I am currently learning React and I am also seeking the
+        opportunities to improve my skills. If you want to take a closer look of
+        what I am up to, check out my Projects page!
+      </p>
     </>
   );
 };
--- my-app/src/Header/Header.tsx
+++ my-app/src/Header/Header.tsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
   return (
-    <nav>
-      <Link to="/">About</Link>
-      <Link to="/skills">Skills</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contact-me">Contact</Link>
-    </nav>
+    <>
+      <h2 className={styles.backgroundTest}>Bohdana Klymenko</h2>
+      <nav>
+        <Link to="/">About</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact-me">Contact</Link>
+      </nav>
+    </>
   );
 };
 
