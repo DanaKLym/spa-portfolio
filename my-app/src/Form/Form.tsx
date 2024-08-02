160b0261df123edf085b780dbeaeb3f49dd1d79e
DanaKLym <bohdanaklymenko@gmail.com>
2024-08-02 17:58:00 +0300

    coded some handling functions to make Form component more controlled

--- my-app/src/Form/Form.tsx
+++ my-app/src/Form/Form.tsx
@@ -1,35 +1,68 @@
+import { useState } from "react";
 import styles from "./Form.module.scss";
 
 const Form: React.FC = () => {
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    message: "",
+    reasons: "",
+  });
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault;
+  };
   return (
     <form>
       <div>
         <label htmlFor="name">Enter your name:</label>
         <br />
-        <input type="text" name="name" id="name" required />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          value={value.name}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="email">Provide your email address:</label>
         <br />
-        <input type="email" name="email" id="email" required />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          value={value.email}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="contactingReasons">Choose topic:</label>
         <br />
-        <input name="contactingReasons" id="contactingReasons" list="reasons" />
-        <datalist id="reasons">
-          <option value="Work query"></option>
-          <option value="Other"></option>
-        </datalist>
+        <select
+          name="reasons"
+          id="contactingReasons"
+          value={value.reasons}
+          onChange={handleChange}
+        >
+          <option value="">Please, choose an option</option>
+          <option value="Work query">Work query</option>
+          <option value="Other">Other</option>
+        </select>
       </div>
       <div>
         <label htmlFor="message">Enter your query:</label>
         <br />
-        <input
-          type="text"
+        <textarea
           name="message"
           id="message"
-          className={styles.messageField}
+          value={value.message}
+          onChange={handleChange}
         />
       </div>
       <div>
